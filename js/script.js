4578f937561809294600c69e0572407d2c507d0f
austinkaruru <karuruaustine@gmail.com>
2018-07-13 08:41:27 +0300

    updated html

--- js/script.js
+++ js/script.js
@@ -2,18 +2,19 @@ $(document).ready(function() {
     $("form#ping-pong").submit(function(event) {
         event.preventDefault();
         var number = parseInt($("input#number").val());
-        var result = pingpong(number);
-        $("#result").text(result);
+        var result = number;
+    
 
-
-        });
         var pingpong = function(number) {
-             $(number % 3 === 0) 
-                return ping;
-             $(number % 5 === 0) 
-                return pong;
-             $(number % 15 === 0)
-                return pingpong;
-            
-        };
-    });
+            if (number % 3 === 0){ 
+                console.log(ping);
+             }; if(number % 5 === 0) 
+                 return pong;
+             }; if(number % 15 ===0) 
+                 return pingpong;
+             });  
+                 
+               
+             }); 
+                 
+
