de5b05f9a07207774516909c73cb44ef54d5df34
austinkaruru <karuruaustine@gmail.com>
2018-07-13 11:13:10 +0300

    changed function in js

--- js/script.js
+++ js/script.js
@@ -2,22 +2,26 @@ $(document).ready(function() {
     $("form#ping-pong").submit(function(event) {
         event.preventDefault();
         var number = parseInt($("input#number").val());
-        var result = number;
-    
-
-        var pingpong = function(number) {
-            if (number % 3 === 0){ 
-                return ping;
-             } else if(number % 5 === 0){ 
-                 return pong;
-             } else if(number % 15 ===0){ 
-                 return pingpong;
-             } else if((number % 3 !==0) && (number % 5 !== 0) && (number % 15 !== 0)) {
-                 return N/A;
-            };
-            };
-            )};
-            )};
+        var result = numbers(number);
+        $("#result").text(result);
+ 
+    });
+});
+     var numbers = function(number) {
+             if((number % 3 ===0) && (number % 5 === 0)) {
+                 return "pingpong";
+            }
+            else if ((number%5)===0) {
+                return "pong";
+            }
+             else if ((number % 3) === 0) {
+                 return "ping";
+             
+            }
+            else {
+                return number;
+            }
+        };
                     
             
             
